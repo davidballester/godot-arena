21ef69d2162b1221e9961fb33bca5f4b5b67dca1
David Ballester Mena <david.ballester.mena@gmail.com>
2024-04-10 21:52:46 +0200

    Fix z-index of combatants

--- controller/battle/battle_controller.gd
+++ controller/battle/battle_controller.gd
@@ -3,6 +3,7 @@ class_name BattleController
 
 var _battle: Battle
 var _id_to_team: Dictionary
+var _id_to_combatant: Dictionary
 
 func _ready() -> void:
 	_battle = Battle.new()
@@ -18,12 +19,21 @@ func _ready() -> void:
 			team.remove_child(combatant)
 			add_combatant(team.id, combatant)
 			
+func _physics_process(_delta: float) -> void:
+	var combatants: Array = _id_to_combatant.values()
+	combatants.sort_custom(func(c0: CombatantController, c1: CombatantController):
+		return c0.global_position.y < c1.global_position.y
+	)
+	for i in range(0, combatants.size()):
+		var combatant: CombatantController = combatants[i]
+		combatant.z_index = i
 	
 func add_team(team: Team) -> void:
 	_id_to_team[team.id] = team
 	add_child(team)
 	
 func add_combatant(team_id: String, combatant: CombatantController) -> void:
+	_id_to_combatant[combatant.id] = combatant
 	var team: Team = _id_to_team[team_id]
 	combatant.team = team
 	combatant.battle = _battle
