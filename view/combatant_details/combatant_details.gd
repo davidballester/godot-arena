c1f96e1b7b5e26fe3689202e57095ecf2e97a9ed
David Ballester Mena <david.ballester.mena@gmail.com>
2024-04-17 16:00:13 +0200

    Add slide animation to details

--- view/combatant_details/combatant_details.gd
+++ view/combatant_details/combatant_details.gd
@@ -1,5 +1,8 @@
-extends Control
+extends Node2D
 class_name CombatantDetails
+
+const WIDTH = 150
+const SLIDE_DURATION_S = 0.15
 
 @onready var _combatant_animated_sprite: AnimatedSprite2D = %CombatantAnimatedSprite
 @onready var _combatant_name_label: Label = %CombatantNameLabel
@@ -10,7 +13,14 @@ class_name CombatantDetails
 @onready var _type_value = %TypeValue
 @onready var _weapon_value = %WeaponValue
 
+@export var slide_offset: int = 16
+
 var _combatant: Combatant
+var _open: bool
+
+func _ready() -> void:
+	position.x = WIDTH + slide_offset
+	_open = false
 
 func show_details(combatant: Combatant, team: Team) -> void:
 	if _combatant:
@@ -20,6 +30,15 @@ func show_details(combatant: Combatant, team: Team) -> void:
 	_health_bar.initialize(combatant.health, team.color)
 	_show_weapon_animated_sprite()
 	_show_labels(team)
+	if not _open:
+		_open = true
+		_slide_in()
+		
+func hide_details() -> void:
+	if not _open:
+		return
+	_open = false
+	_slide_out()
 
 func _on_combatant_state_changed(state: State) -> void:
 	if state is CombatantDeadState:
@@ -30,10 +49,9 @@ func _on_combatant_state_changed(state: State) -> void:
 		return
 
 func _input(event) -> void:
-	if not visible:
+	if not visible or not event.is_action_pressed("escape"):
 		return
-	if event.is_action_pressed("escape"):
-		hide()
+	hide_details()
 		
 func _process(_delta: float) -> void:
 	if not visible or not _combatant:
@@ -74,3 +92,21 @@ func _show_labels(team: Team) -> void:
 	_type_value.add_theme_color_override("font_color", team.color)
 	_weapon_value.text = _combatant.weapon.weapon_name.capitalize()
 	_weapon_value.add_theme_color_override("font_color", team.color)
+
+func _slide_in() -> void:
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property(
+		self, 
+		"position", 
+		Vector2(0, 0), 
+		SLIDE_DURATION_S
+	).set_ease(Tween.EASE_OUT)
+
+func _slide_out() -> void:
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property(
+		self, 
+		"position", 
+		Vector2(WIDTH + slide_offset, 0), 
+		SLIDE_DURATION_S
+	).set_ease(Tween.EASE_IN)
--- view/hud/hud.gd
+++ view/hud/hud.gd
@@ -5,13 +5,12 @@ class_name HUD
 @onready var _second_team_score: TeamScore = %SecondTeamScore
 @onready var _combatant_details: CombatantDetails = %CombatantDetails
 
-func _ready() -> void:
-	_combatant_details.hide()
-
 func initialize(first_team: Team, second_team: Team) -> void:
 	_first_team_score.initialize(first_team, first_team.color)
 	_second_team_score.initialize(second_team, second_team.color)
 
 func show_combatant_details(combatant: Combatant, team: Team) -> void:
 	_combatant_details.show_details(combatant, team)
-	_combatant_details.show()
+
+func hide_combatant_details() -> void:
+	_combatant_details.hide_details()
