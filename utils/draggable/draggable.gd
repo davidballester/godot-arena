22831a5ed09d4c72800f6dd87ea39532490ceb8d
David Ballester Mena <david.ballester.mena@gmail.com>
2024-05-08 16:38:24 +0200

    Add dragging of UI elements

---
+++ utils/draggable/draggable.gd
@@ -0,0 +1,31 @@
+extends BaseButton
+class_name Draggable
+
+@export var movable_control: Control
+
+var _last_mouse_position: Vector2
+var _is_dragging: bool
+
+func _ready() -> void:
+	button_down.connect(func(): 
+		_is_dragging = true
+		_last_mouse_position = get_viewport().get_mouse_position()
+	)
+	button_up.connect(func(): _is_dragging = false)
+
+func _process(_delta: float) -> void:
+	if not _is_dragging:
+		return
+	var mouse_position = get_viewport().get_mouse_position()
+	var delta = mouse_position - _last_mouse_position
+	var new_x = movable_control.global_position.x + delta.x
+	var new_y = movable_control.global_position.y + delta.y
+	if new_x + movable_control.size.x > GameGlobals.VIEWPORT_WIDTH:
+		new_x = GameGlobals.VIEWPORT_WIDTH - movable_control.size.x
+	if new_y + movable_control.size.y > GameGlobals.VIEWPORT_HEIGHT:
+		new_y = GameGlobals.VIEWPORT_HEIGHT - movable_control.size.y
+	movable_control.global_position = Vector2(
+		max(0, new_x),
+		max(0, new_y),
+	)
+	_last_mouse_position = mouse_position
