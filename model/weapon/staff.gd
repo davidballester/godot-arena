eaea1ffa95e29146090915ee37c099950c53819e
David Ballester Mena <david.ballester.mena@gmail.com>
2024-05-15 20:53:14 +0200

    Remove unused models

--- model/weapon/staff.gd
+++
@@ -1,9 +0,0 @@
-extends Weapon
-class_name Staff
-
-func _ready() -> void:
-	damage = MinMax.new()
-	damage.min_value = 1
-	damage.max_value = 1
-	attack_duration_s = 0.8
-	reach = 35
--- model/weapon/sword.gd
+++
@@ -1,9 +0,0 @@
-extends Weapon
-class_name Sword
-
-func _ready() -> void:
-	damage = MinMax.new()
-	damage.min_value = 1
-	damage.max_value = 2
-	attack_duration_s = 1.0
-	reach = 30
