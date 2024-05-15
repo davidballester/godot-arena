071feaf78da6a11f562d45ade13b056a6c9acd50
David Ballester Mena <david.ballester.mena@gmail.com>
2024-05-15 20:53:09 +0200

    Fix weapons rotation

--- view/weapon/weapon_view.gd
+++ view/weapon/weapon_view.gd
@@ -12,8 +12,8 @@ func attack() -> void:
 	pass
 	
 func face_position(g_position: Vector2) -> void:
-	rotation = global_position.angle_to_point(g_position)
-
+	rotation = 0.0 if g_position.x > global_position.x and scale.y > 0 else PI
+	
 func idle() -> void:
 	animated_sprite.play("idle")
 
