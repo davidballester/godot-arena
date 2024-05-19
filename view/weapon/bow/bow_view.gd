2620507291fd81a70f3401a9ce6c47abe7ef76fa
David Ballester Mena <david.ballester.mena@gmail.com>
2024-05-19 20:21:58 +0200

    Add bow

---
+++ view/weapon/bow/bow_view.gd
@@ -0,0 +1,21 @@
+extends WeaponView
+class_name BowView
+
+var _attacking: bool = false
+
+func _ready() -> void:
+	animated_sprite = %AnimatedSprite2D
+	super._ready()
+
+func attack(g_position: Vector2) -> void:
+	_attacking = true
+	rotation = global_position.angle_to_point(g_position)
+	animated_sprite.play("shooting")
+	await animated_sprite.animation_finished
+	idle()
+	_attacking = false
+	
+func face_position(g_position: Vector2) -> void:
+	if _attacking:
+		return
+	super.face_position(g_position)
